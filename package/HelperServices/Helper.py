7495c6dbfe310fe3805d6d346e561840f2b39cb4
nguyenvu <nvupham@maxis.com.my>
2022-04-26 03:50:57 +0700

    Update sudo command

--- package/HelperServices/Helper.py
+++ package/HelperServices/Helper.py
@@ -14,19 +14,19 @@ def inputContentToFile(fileLocation, content):
 def outputObjectFromUnscannedToMachine(fileLocation,objectName ):
     subprocess.Popen(['ls', '-la'], shell=False)
     print("\nGet object to scanned folder")
-    subprocess.Popen(['rados get', f'{objectName}',f'{fileLocation}{objectName}','--pool=for-scan'])
+    subprocess.Popen(['sudo','rados get', f'{objectName}',f'{fileLocation}{objectName}','--pool=for-scan'])
     print("\nScan file and change name file(scanned) if virus annouce to user")
     cd = pyclamd.ClamdAgmpstic()
     result = cd.scan_file(f'{fileLocation}{objectName}')
     if result is None:
         os.rename(f'{fileLocation}{objectName}',f'{fileLocation}{objectName}(CLEAN)')
         print("\nPut clean scanned object to pool")
-        subprocess.Popen(['rados put', f'{fileLocation}{objectName}(CLEAN)', f'{fileLocation}{objectName}(CLEAN)','--pool=cephfs-data'])
+        subprocess.Popen(['sudo','rados put', f'{fileLocation}{objectName}(CLEAN)', f'{fileLocation}{objectName}(CLEAN)','--pool=cephfs-data'])
         return 1
 
     else:
         print(f'{result}')
         os.rename(f'{fileLocation}{objectName}',f'{fileLocation}{objectName}(NOT-CLEAN)')
         print("\nPut not clean scanned object to pool")
-        subprocess.Popen(['rados put', f'{fileLocation}{objectName}(NOT-CLEAN)',f'{fileLocation}{objectName}(NOT-CLEAN)','--pool=cephfs-data'])
+        subprocess.Popen(['sudo','rados put', f'{fileLocation}{objectName}(NOT-CLEAN)',f'{fileLocation}{objectName}(NOT-CLEAN)','--pool=cephfs-data'])
         return result
